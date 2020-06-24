b22507025bf2cbe1f640c1ab6073ce202ec33077
yeoseon <devyyskr@gmail.com>
2020-06-25 08:59:50 +0900

    feat(MockExam): 결과 처리 제외하고 완성 (배열에 동적으로 길이 늘려서 원소 넣기

--- src/MockExam.java
+++ src/MockExam.java
@@ -20,8 +20,8 @@ public class MockExam {
         int count = 0;
 
         for(int i = 0; i < answers.length; i++) {
-            if(i % 5 != 0) {
-                if(answers[i] == i % 5) {
+            if((i+1) % 5 != 0) {
+                if(answers[i] == (i+1) % 5) {
                     count++;
                 }
             }
@@ -38,28 +38,28 @@ public class MockExam {
         int count = 0;
 
         for(int i = 0; i < answers.length; i++) {
-            if(i % 2 == 1) {
+            if((i+1) % 2 == 1) {
                 if(answers[i] == 2) {
                     count++;
                 }
             }
             else {
-                if(i % 8 == 2) {
+                if((i+1) % 8 == 2) {
                     if(answers[i] == 1) {
                         count++;
                     }
                 }
-                else if(i % 8 == 4) {
+                else if((i+1) % 8 == 4) {
                     if(answers[i] == 3) {
                         count++;
                     }
                 }
-                else if(i % 8 == 6) {
+                else if((i+1) % 8 == 6) {
                     if(answers[i] == 4) {
                         count++;
                     }
                 }
-                else if(i % 8 == 0) {
+                else if((i+1) % 8 == 0) {
                     if(answers[i] == 5) {
                         count++;
                     }
@@ -73,22 +73,22 @@ public class MockExam {
         int count = 0;
 
         for(int i = 0; i < answers.length; i++) {
-            if(i % 10 == 1 || i % 10 == 2) {
+            if((i+1) % 10 == 1 || (i+1) % 10 == 2) {
                 if(answers[i] == 3) {
                     count++;
                 }
             }
-            else if(i % 10 == 3 || i % 10 == 4) {
+            else if((i+1) % 10 == 3 || (i+1) % 10 == 4) {
                 if(answers[i] == 1) {
                     count++;
                 }
             }
-            else if(i % 10 == 5 || i % 10 == 6) {
+            else if((i+1) % 10 == 5 || (i+1) % 10 == 6) {
                 if(answers[i] == 2) {
                     count++;
                 }
             }
-            else if(i % 10 == 7 || i % 10 == 8) {
+            else if((i+1) % 10 == 7 || (i+1) % 10 == 8) {
                 if(answers[i] == 4) {
                     count++;
                 }
@@ -104,9 +104,9 @@ public class MockExam {
 
     private int[] getMaxCount(int count1, int count2, int count3) {
         int[] countResult = {count1, count2, count3};
-        int[] result = new int[3];
-        List<Integer> tempResult = new ArrayList<>();
+        int[] result = {};
 
+        //Get Max Count
         int maxCount = countResult[0];
 
         for(int i = 0; i < countResult.length; i++) {
@@ -115,15 +115,19 @@ public class MockExam {
             }
         }
 
+        int index = 0;
+
         for(int i = 0; i < countResult.length; i++) {
             if(countResult[i] == maxCount) {
-                tempResult.add(i + 1);
-            }
-        }
+                //배열 길이 늘려서 넣기
+                if(result.length < index + 1) {
+                    int[] tempResult = new int[result.length + index + 1];
+                    tempResult[result.length + index] = i + 1;
+                    result = tempResult;
 
-        int size = 0;
-        for(int temp : tempResult) {
-            result[size++] = temp;
+                    index++;
+                }
+            }
         }
         return result;
     }
--- test/MockExamTest.java
+++ test/MockExamTest.java
@@ -4,6 +4,10 @@ public class MockExamTest {
 
     @Test
     void solution() {
+        int[] answers = {1, 2, 3, 4, 5};
+        int[] answers2 = {1, 3, 2, 4, 2};
+        MockExam mockExam = new MockExam();
 
+        int[] result = mockExam.solution(answers2);
     }
 }

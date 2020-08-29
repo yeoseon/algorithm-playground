16b0eb0dbee433fdacf6f1dbf60bf4be63ca34b2
Yeoseon Yun <devyyskr@gmail.com>
2020-08-30 06:35:43 +0900

    setting(EnglishWordChain): 메인 메소드 선언

--- src/main/java/Solutions/EnglishWordChain.java
+++ src/main/java/Solutions/EnglishWordChain.java
@@ -1,4 +1,16 @@
 package Solutions;
 
+/**
+ * 영어 끝말잇기
+ * https://school.programmers.co.kr/courses/10586/lessons/67680
+ */
 public class EnglishWordChain {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {};
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
 }

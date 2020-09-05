09a6860d6a654a3b6a3268639d6c6206ce995e73
Yeoseon Yun <devyyskr@gmail.com>
2020-09-05 21:41:45 +0900

    test(EnglishWordChainTest): 테스트 케이스 작성

--- src/main/java/Solutions/EnglishWordChain.java
+++ src/main/java/Solutions/EnglishWordChain.java
@@ -5,11 +5,8 @@ package Solutions;
  * https://school.programmers.co.kr/courses/10586/lessons/67680
  */
 public class EnglishWordChain {
-    public int[] solution(int n, String[] words) {
+    public static int[] solution(int n, String[] words) {
         int[] answer = {};
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
 
         return answer;
     }
--- src/test/java/Solutions/EnglishWordChainTest.java
+++ src/test/java/Solutions/EnglishWordChainTest.java
@@ -2,9 +2,31 @@ package Solutions;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class EnglishWordChainTest {
 
     @Test
-    void solution() {
+    void solution1() {
+        int n = 3;
+        String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+
+        assertThat(EnglishWordChain.solution(n, words)).containsExactly(3, 3);
+    }
+
+    @Test
+    void solution2() {
+        int n = 5;
+        String[] words = {"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"};
+
+        assertThat(EnglishWordChain.solution(n, words)).containsExactly(0, 0);
+    }
+
+    @Test
+    void solution3() {
+        int n = 2;
+        String[] words = {"hello", "one", "even", "never", "now", "world", "draw"};
+
+        assertThat(EnglishWordChain.solution(n, words)).containsExactly(1, 3);
     }
 }

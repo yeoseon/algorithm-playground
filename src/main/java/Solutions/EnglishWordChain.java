ef2a2cbe2f48cef3887099992a4091418b203556
Yeoseon Yun <devyyskr@gmail.com>
2020-09-15 18:10:32 +0900

    feat(WordChainGame): 게임 생성 및 단어 assign 기능 구현

--- src/main/java/Solutions/EnglishWordChain.java
+++ src/main/java/Solutions/EnglishWordChain.java
@@ -1,13 +1,95 @@
 package Solutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 영어 끝말잇기
  * https://school.programmers.co.kr/courses/10586/lessons/67680
  */
 public class EnglishWordChain {
-    public static int[] solution(int n, String[] words) {
-        int[] answer = {};
 
+    /**
+     * [탈락의 경우]
+     * 1. 끝말잇기의 규칙에 어긋나는 경우 (끝 문자로 시작하는 단어를 말하지 않은 경우)
+     * 2. 중복되는 단어를 말한 경우
+     *
+     * [구현 방법 구상]
+     * 1. Player 라는 객체를 가진다.
+     * - 가지고 있는 단어 목록과, 순서, player number 정보를 갖는다.
+     * 3. 끝말잇기 게임 객체도 하나 가진다.
+     * - 단어가 적합한지 여부를 판단한다.
+     * - 이를 위해 끝글자 및 history도 관리해야 한다.
+     */
+    public int[] solution(int n, String[] words) {
+        int[] answer = {0, 0};
+
+        WordChainGame game = new WordChainGame(n, words);
         return answer;
+    }
+
+    public WordChainGame makeGame(int n, String[] words) {
+        return new WordChainGame(n, words);
+    }
+
+    public class Player {
+        private int number;         // Player Number (start with 1)
+        private String[] words;     // 가진 단어들
+        private int order;          // 단어 제시 순서
+
+        public Player(int number, String[] words) {
+            this.number = number;
+            this.words = words;
+            this.order = 0;
+        }
+
+        public String[] getWords() {
+            return words;
+        }
+    }
+
+    public class WordChainGame {
+        private int playerCount;        // 게임 참여 Player 수
+        private String[] words;         // 전체 단어
+        private List<Player> players;
+
+        public WordChainGame(int playerCount, String[] words) {
+            this.playerCount = playerCount;
+            this.words = words;
+            this.players = assignPlayers();
+        }
+
+        private List<Player> assignPlayers() {
+            List<Player> players = new ArrayList<>();
+
+            for(int i = 1; i <= playerCount; i++) {
+                players.add(new Player(i, pickPlayerWords(this.playerCount, i, this.words)));
+            }
+
+            return players;
+        }
+
+        // TODO: 더 효율적으로 바꿀 수 있을 것 같다. (Stream을 이용할 수 있을 것 같다.)
+        private String[] pickPlayerWords(int playerCount, int playerNumber, String[] entireWords) {
+            List<String> words = new ArrayList<>();
+
+            for(int i = 0; i < entireWords.length; i++) {
+                if(playerCount == playerNumber) {
+                    if((i+1) % playerCount == 0) {
+                        words.add(entireWords[i]);
+                    }
+                }
+                else if((i+1) % playerCount == playerNumber) {
+                    words.add(entireWords[i]);
+                }
+            }
+
+            return words.stream()
+                    .toArray(String[]::new);
+        }
+
+        public List<Player> getPlayers() {
+            return players;
+        }
     }
 }
--- src/test/java/Solutions/EnglishWordChainTest.java
+++ src/test/java/Solutions/EnglishWordChainTest.java
@@ -1,17 +1,22 @@
 package Solutions;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnglishWordChainTest {
+
+    EnglishWordChain instance = new EnglishWordChain();
 
     @Test
     void solution1() {
         int n = 3;
         String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
 
-        assertThat(EnglishWordChain.solution(n, words)).containsExactly(3, 3);
+        assertThat(instance.solution(n, words)).containsExactly(3, 3);
     }
 
     @Test
@@ -19,7 +24,7 @@ public class EnglishWordChainTest {
         int n = 5;
         String[] words = {"hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"};
 
-        assertThat(EnglishWordChain.solution(n, words)).containsExactly(0, 0);
+        assertThat(instance.solution(n, words)).containsExactly(0, 0);
     }
 
     @Test
@@ -27,6 +32,20 @@ public class EnglishWordChainTest {
         int n = 2;
         String[] words = {"hello", "one", "even", "never", "now", "world", "draw"};
 
-        assertThat(EnglishWordChain.solution(n, words)).containsExactly(1, 3);
+        assertThat(instance.solution(n, words)).containsExactly(1, 3);
+    }
+
+    @Test
+    @DisplayName("게임을 생성할 시, 플레이어의 단어 목록이 잘 Assign 되는지 확인한다.")
+    void WordChainGame() {
+        int n = 3;
+        String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+
+        EnglishWordChain.WordChainGame game = instance.makeGame(n, words);
+        List<EnglishWordChain.Player> players = game.getPlayers();
+
+        assertThat(players.get(0).getWords()).containsExactly("tank", "wheel", "mother");
+        assertThat(players.get(1).getWords()).containsExactly("kick", "land", "robot");
+        assertThat(players.get(2).getWords()).containsExactly("know", "dream", "tank");
     }
 }

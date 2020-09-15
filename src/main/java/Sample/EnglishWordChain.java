56558d2a4755c35278ab0c2c368ed1fd0a4f2138
Yeoseon Yun <devyyskr@gmail.com>
2020-09-15 19:31:54 +0900

    sample(EnglishWordChain): 샘플 코드 추가 (간단하네 ㅠㅠ)

---
+++ src/main/java/Sample/EnglishWordChain.java
@@ -0,0 +1,35 @@
+package Sample;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EnglishWordChain {
+    public int[] solution(int n, String[] words) {
+
+        int personNum = 1;
+        int personOrder = 1;
+
+        Set<String> passedWords = new HashSet<>();
+
+        String beforeWord = words[0];
+
+        for (int i = 1; i < words.length; i++) {
+            String nextWord = words[i];
+            passedWords.add(beforeWord);
+
+            personNum = (personNum % n) + 1;
+
+            if (i % n == 0) {
+                personOrder++;
+            }
+
+            if (passedWords.contains(nextWord) || beforeWord.charAt(beforeWord.length() - 1) != nextWord.charAt(0)) {
+                return new int[]{personNum, count};
+            }
+
+            beforeWord = nextWord;
+        }
+
+        return new int[]{0, 0};
+    }
+}

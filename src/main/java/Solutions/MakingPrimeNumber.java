432eedfcb23a9b402b4cf3eeac38a1bdace02a3b
Yeoseon Yun <devyyskr@gmail.com>
2020-09-19 15:25:35 +0900

    test(MakingPrimeNumberTest): 테스트 메소드 추가

--- src/main/java/Solutions/MakingPrimeNumber.java
+++ src/main/java/Solutions/MakingPrimeNumber.java
@@ -1,4 +1,13 @@
 package Solutions;
 
 public class MakingPrimeNumber {
+
+    public int solution(int[] nums) {
+        int answer = -1;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
 }
--- src/test/java/Solutions/MakingPrimeNumberTest.java
+++ src/test/java/Solutions/MakingPrimeNumberTest.java
@@ -1,4 +1,24 @@
 package Solutions;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MakingPrimeNumberTest {
+
+    @Test
+    void solutionTest1() {
+        MakingPrimeNumber instance = new MakingPrimeNumber();
+        int[] nums = new int[]{1, 2, 3, 4};
+
+        assertThat(instance.solution(nums)).isEqualTo(1);
+    }
+
+    @Test
+    void solutionTest2() {
+        MakingPrimeNumber instance = new MakingPrimeNumber();
+        int[] nums = new int[]{1, 2, 7, 6, 4};
+
+        assertThat(instance.solution(nums)).isEqualTo(4);
+    }
 }

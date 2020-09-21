174f40410b638b153d467a98f0aa66d4f0bbf9a5
Yeoseon Yun <devyyskr@gmail.com>
2020-09-21 13:53:23 +0900

    solving(MakingPrimeNumber): 소수만들기 풀이 1 추가

--- src/main/java/Solutions/MakingPrimeNumber.java
+++ src/main/java/Solutions/MakingPrimeNumber.java
@@ -1,15 +1,116 @@
 package Solutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MakingPrimeNumber {
 
-    // 3개를 뽑아내는 경우의 수를 모두 탐색해서 푼다?
-    // 소수임을 어떻게 알지
-    public int solution(int[] nums) {
-        int answer = -1;
+    /**
+     * solution1 : 단순 풀이
+     * 3중 포문을 이용해 모두 더한다.
+     * @param nums
+     * @return
+     */
+    public int solution1(int[] nums) {
+        int answer = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        for(int i = 0; i < nums.length - 2; i++) {
+            for(int j = i + 1; j < nums.length - 1; j++) {
+                for(int k = j + 1; k < nums.length; k++) {
+                    if(isPrimeNumber1(nums[i] + nums[j] + nums[k])) {
+                        answer++;
+                    }
+                }
+            }
+        }
 
         return answer;
+    }
+
+    /**
+     * isPrimeNumber1 - 소수 판별 함수 1
+     * 1을 제외한 모든 수를 나눠서 판단한다.
+     * 시간 복잡도는 O(N)
+     * @param number
+     * @return
+     */
+    public boolean isPrimeNumber1(int number) {
+        if(number <= 1) {
+            return false;
+        }
+
+        for(int i = 2; i < number; i++) {
+            if(number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * isPrimeNumber2 - 소수 판별 함수 2
+     * 해당 숫자의 절반까지만 확인하는 방법
+     * 절반 이상의 숫자는 확인이 필요 없다.
+     * 이 또한 시간복잡도는 O(N)이 된다. - N/2번 조회
+     *
+     * @param number
+     * @return
+     */
+    public boolean isPrimeNumber2(int number) {
+        if(number <= 1) {
+            return false;
+        }
+
+        for(int i = 2; i <= number/2; i++) {
+            if(number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * isPrimeNumber3 - 소수 판별 함수 3
+     * 제곱근을 이용한다. - https://st-lab.tistory.com/80
+     * 시간 복잡도는 O(sqrt(N))이 된다.
+     * @param number
+     * @return
+     */
+    public boolean isPrimeNumber3(int number) {
+        if(number <= 1) {
+            return false;
+        }
+
+        for(int i = 2; i*i <= number; i++) {
+            if(number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * makePrimeNumber - 소수 판별 함수 4
+     * 에라토스테네스의 체 (https://marobiana.tistory.com/91, https://st-lab.tistory.com/80)
+     * 이 방법은, 소수 여부를 판별하기 보다는 현재 모든 수 중 소수만을 걸러서 구하는 상황에 쓰인다.
+     * 현재 문제에서는 true/false 여부만 알면 되기 때문에 사용하지는 않지만, 참고 용으로 Sample 코드를 가져온다.
+     *
+     * @param number
+     * @return
+     */
+    public void makePrimeNumber(int number) {
+        boolean[] primeArray = new boolean[3001];
+
+        for (int i = 2; i < primeArray.length; i++) {
+            primeArray[i] = true;
+        }
+
+        for (int i = 2; i < primeArray.length; i++) {
+            for (int j = i * i; j < primeArray.length; j += i)
+                primeArray[j] = false;
+        }
     }
 }
--- src/test/java/Solutions/MakingPrimeNumberTest.java
+++ src/test/java/Solutions/MakingPrimeNumberTest.java
@@ -1,5 +1,6 @@
 package Solutions;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,7 +12,7 @@ public class MakingPrimeNumberTest {
         MakingPrimeNumber instance = new MakingPrimeNumber();
         int[] nums = new int[]{1, 2, 3, 4};
 
-        assertThat(instance.solution(nums)).isEqualTo(1);
+        assertThat(instance.solution1(nums)).isEqualTo(1);
     }
 
     @Test
@@ -19,6 +20,31 @@ public class MakingPrimeNumberTest {
         MakingPrimeNumber instance = new MakingPrimeNumber();
         int[] nums = new int[]{1, 2, 7, 6, 4};
 
-        assertThat(instance.solution(nums)).isEqualTo(4);
+        assertThat(instance.solution1(nums)).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("소수인지 판별한다.")
+    void isPrimeNumber() {
+        MakingPrimeNumber instance = new MakingPrimeNumber();
+
+        //TODO: assertJ를 이용해 더 편리하게 Refactoring 한다.
+        assertThat(instance.isPrimeNumber1(0)).isFalse();
+        assertThat(instance.isPrimeNumber1(1)).isFalse();
+        assertThat(instance.isPrimeNumber1(2)).isTrue();
+        assertThat(instance.isPrimeNumber1(10)).isFalse();
+        assertThat(instance.isPrimeNumber1(13)).isTrue();
+
+        assertThat(instance.isPrimeNumber2(0)).isFalse();
+        assertThat(instance.isPrimeNumber2(1)).isFalse();
+        assertThat(instance.isPrimeNumber2(2)).isTrue();
+        assertThat(instance.isPrimeNumber2(10)).isFalse();
+        assertThat(instance.isPrimeNumber2(13)).isTrue();
+
+        assertThat(instance.isPrimeNumber3(0)).isFalse();
+        assertThat(instance.isPrimeNumber3(1)).isFalse();
+        assertThat(instance.isPrimeNumber3(2)).isTrue();
+        assertThat(instance.isPrimeNumber3(10)).isFalse();
+        assertThat(instance.isPrimeNumber3(13)).isTrue();
     }
 }

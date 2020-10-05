5ef698b530b60472fda62e244c9ab03d84914e0b
Yeoseon Yun <devyyskr@gmail.com>
2020-10-05 13:54:33 +0900

    solve(SkillTree): 내 방법대로 SkillTree 풀이

--- src/main/java/Solutions/SkillTree.java
+++ src/main/java/Solutions/SkillTree.java
@@ -1,8 +1,84 @@
 package Solutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SkillTree {
-    public int solution(String skill, String[] skill_trees) {
+    /**
+     * 1. 주어진 배열을 for문으로 돌려 검사한다.
+     * 2. 검사 조건
+     *  - 입력받은 skill의 순서가 맞아야 한다.
+     *  - 중간에 입력받은 skill 외 skill이 있어도 된다.
+     *  - 입력받은 skill이 모두 있지 않아도 된다. 아예 없어도 된다.
+     * 3. 구현 방법
+     *  - 1) 중간에 입력받은 skill 외 skill은 모두 신경쓰지 않기 때문에 모두 제외한다.
+     *  - 2) 남은 것이 없는 경우 True 이다.
+     *  - 3) 남은 것이 있는 경우
+     *      - 3-2) 입력받은 수를 차례대로 비교해서 다른 경우 False, 같은 경우 True (끝까지 가지 않아도 순서만 같으면 됨)
+     * 4. 그 외 고민
+     *  - 입력받은 skill로 순서대로 구성하는 경우의 수를 담아서 만들고, 위의 3의 1) 후에 포함되는지 여부만 판단하면 되지 않을까 싶다.
+     * @param skill
+     * @param skill_trees
+     * @return
+     */
+    public int solution1(String skill, String[] skill_trees) {
         int answer = 0;
+
+        for(int i = 0; i < skill_trees.length; i++) {
+            if(isPossibleSkillTree(removeExceptSkill(skill_trees[i], skill), skill)) {
+                answer++;
+            }
+        }
         return answer;
+    }
+
+    /**
+     * 입력받은 Skill 외 Skill 삭제
+     * @param skill_tree
+     * @param skill
+     * @return
+     */
+    public String removeExceptSkill(String skill_tree, String skill) {
+        List<String> removeTargets = new ArrayList<>();
+
+        for(int i = 0; i < skill_tree.length(); i++) {
+            int count = 0;
+
+            for(int j = 0; j < skill.length(); j++) {
+                if(skill_tree.charAt(i) == skill.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if(count <= 0) {
+                removeTargets.add(Character.toString(skill_tree.charAt(i)));
+            }
+        }
+
+        // skill_tree.charAt(i)를 remove한다.
+        for(String removeTarget : removeTargets) {
+            skill_tree = skill_tree.replaceAll(removeTarget, "");
+        }
+
+        return skill_tree;
+    }
+
+    /**
+     * 가능한 스킬트리인지 검사
+     * @param skill_tree
+     * @param skill
+     * @return
+     */
+    private boolean isPossibleSkillTree(String skill_tree, String skill) {
+        if(skill.length() == 0) {
+            return true;
+        }
+
+        for(int i = 0; i < skill_tree.length(); i++) {
+            if(skill_tree.charAt(i) != skill.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/Solutions/SkillTreeTest.java
+++ src/test/java/Solutions/SkillTreeTest.java
@@ -13,6 +13,16 @@ public class SkillTreeTest {
         String skill = "CBD";
         String[] skillTrees = {"BACDE", "CBADF", "AECB", "BDA"};
 
-        assertThat(instance.solution(skill, skillTrees)).isEqualTo(2);
+        assertThat(instance.solution1(skill, skillTrees)).isEqualTo(2);
+    }
+
+    @Test
+    void removeExceptSkill() {
+        SkillTree instance = new SkillTree();
+
+        String skill_tree = "AECB";
+        String skill = "CBD";
+
+        assertThat(instance.removeExceptSkill(skill_tree, skill)).isEqualTo("CB");
     }
 }

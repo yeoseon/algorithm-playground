4c2fb6624e38b68dffbdb89b609b871c87035667
Yeoseon Yun <devyyskr@gmail.com>
2020-10-10 13:56:45 +0900

    solve(PriceOfStock): 불필요한 List 사용 삭제

--- src/main/java/Solutions/PriceOfStock.java
+++ src/main/java/Solutions/PriceOfStock.java
@@ -1,42 +1,44 @@
 package Solutions;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PriceOfStock {
 
     /**
-     * i = 1부터 시작한다.
+     * Solution 1 : 단순 풀이
+     *
+     * i = 0부터 시작한다.
      *
      * 1. 끝까지 안떨어지는 경우 (최소 숫자)
-     * => length - i
+     * => length - i - 1
      * 2. 떨어지는 경우 (뒤에 최초로 작은 숫자가 나온다면)
      * => 작은 숫자의 i - 나의 i
      *
-     * index 와 value를 다뤄보면 되지 않을까?
      *
      * @param prices
      * @return
      */
     public int[] solution1(int[] prices) {
-        List<Integer> answerList = new ArrayList<>();
+//        List<Integer> answerList = new ArrayList<>();
+        int[] answer = new int[prices.length];
 
         for(int i = 0; i < prices.length; i++) {
             boolean isDown = false;
             for(int j = i+1; j < prices.length; j++) {
                 if(prices[j] < prices[i]) {
-                    answerList.add(j - i);
+//                    answerList.add(j - i);
+                    answer[i] = j - i;
                     isDown = true;
                     break;
                 }
             }
             if(!isDown) {
-                answerList.add(prices.length - i - 1);
+//                answerList.add(prices.length - i - 1);
+                answer[i] = prices.length - i - 1;
             }
         }
 
-        return answerList.stream()
-                    .mapToInt(i -> i)
-                    .toArray();
+//        return answerList.stream()
+//                    .mapToInt(i -> i)
+//                    .toArray();
+        return answer;
     }
 }

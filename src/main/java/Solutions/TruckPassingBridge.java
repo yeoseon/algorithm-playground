d96390afe8354a30b717a505641b6347cddecc7a
Yeoseon Yun <devyyskr@gmail.com>
2020-10-12 15:35:36 +0900

    solve(TruckPassingBridge): 1차 풀이 (실패)

---
+++ src/main/java/Solutions/TruckPassingBridge.java
@@ -0,0 +1,89 @@
+package Solutions;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Stream;
+
+public class TruckPassingBridge {
+
+    /**
+     * 1. Queue를 이용한다.
+     * 2. Truck이라는 클래스를 만든다.
+     *  * truck_weight
+     *  * 지나온 길이 (1초당 1씩)
+     *  * 현재 시간을 받아 다리 건너는 것을 완료했는 지의 여부
+     * 3. 그 외 필요 변수 -> 이것조 Bridge Class 만들자.
+     *  * 현재 다리의 weight
+     *  * 현재 시간 (초)
+     *  * 다리가 버틸 수 있는 weight (주어짐)
+     * @param bridge_length
+     * @param weight
+     * @param truck_weights
+     * @return
+     */
+    public int solution1(int bridge_length, int weight, int[] truck_weights) {
+        List<Truck> truckList = new ArrayList<>();
+        List<Truck> passedTruckList = new ArrayList<>();
+
+        for(int truck_weight : truck_weights) {
+            truckList.add(new Truck(truck_weight));
+        }
+
+        Queue<Truck> passingTrucks = new LinkedList<>();
+        int second = 0;
+        int weightPassingBridge = 0;       // 현재 다리 위에 올라온 Truck의 무게
+
+        for(int i = 0; i < truckList.size() && truckList.size() > passedTruckList.size();) {
+            second++;
+
+            if(weightPassingBridge + truckList.get(i).getTruck_weight() <= weight) {
+                passingTrucks.add(truckList.get(i));
+                weightPassingBridge += truckList.get(i).getTruck_weight();
+
+                i++;        // 이 로직대로라면 i는 size만큼 올라간다.
+            }
+
+            for(Truck passingTruck : passingTrucks) {
+                passingTruck.go();
+            }
+
+            if(passingTrucks.peek().isPassed(bridge_length)) {
+                Truck passedTruck = passingTrucks.poll();
+                passedTruckList.add(passedTruck);
+                weightPassingBridge -= passedTruck.getTruck_weight();
+            }
+        }
+
+        return second;
+    }
+
+    public class Truck {
+        private int truck_weight;   // 트럭의 무게
+        private int passingLength;  // 지나간 길이 (1초의 1씩)
+
+        public Truck(int truck_weight) {
+            this.truck_weight = truck_weight;
+            this.passingLength = 0;
+        }
+
+        public int getTruck_weight() {
+            return truck_weight;
+        }
+
+        public void go() {
+            this.passingLength++;
+        }
+
+        /**
+         * 다리의 길이를 받아, 본 Truck이 다리를 모두 지나왔는지 확인한다.
+         * @param bridge_length
+         * @return
+         */
+        public boolean isPassed(int bridge_length) {
+            return bridge_length == passingLength;
+        }
+    }
+}
---
+++ src/test/java/Solutions/TruckPassingBridgeTest.java
@@ -0,0 +1,28 @@
+package Solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TruckPassingBridgeTest {
+
+    @Test
+    void solution1() {
+        TruckPassingBridge instance = new TruckPassingBridge();
+        int bridge_length1 = 2;
+        int weight1 = 10;
+        int[] truck_weights1 = {7, 4, 5, 6};
+
+        int bridge_length2 = 100;
+        int weight2 = 100;
+        int[] truck_weights2 = {10};
+
+        int bridge_length3 = 100;
+        int weight3 = 100;
+        int[] truck_weights3 = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+
+        assertThat(instance.solution1(bridge_length1, weight1, truck_weights1)).isEqualTo(8);
+        assertThat(instance.solution1(bridge_length2, weight2, truck_weights2)).isEqualTo(101);
+        assertThat(instance.solution1(bridge_length3, weight3, truck_weights3)).isEqualTo(110);
+    }
+}

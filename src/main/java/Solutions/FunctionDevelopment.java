efa70443e1156450bbf75b9ac5cbbea341e171f4
Yeoseon Yun <devyyskr@gmail.com>
2020-10-12 12:48:41 +0900

    test(FunctionDevelopment): 기능개발 문제 풀이 테스트 코드 추가

---
+++ src/main/java/Solutions/FunctionDevelopment.java
@@ -0,0 +1,8 @@
+package Solutions;
+
+public class FunctionDevelopment {
+    public int[] solution1(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+}
---
+++ src/test/java/Solutions/FunctionDevelopmentTest.java
@@ -0,0 +1,21 @@
+package Solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FunctionDevelopmentTest {
+
+    @Test
+    void solution1() {
+        FunctionDevelopment instance = new FunctionDevelopment();
+        int[] progresses1 = {93, 30, 55};
+        int[] speeds1 = {1, 30, 5};
+
+        int[] progresses2 = {95, 90, 99, 99, 80, 99};
+        int[] speeds2 = {1, 1, 1, 1, 1, 1};
+
+        assertThat(instance.solution1(progresses1, speeds1)).containsExactly(2, 1);
+        assertThat(instance.solution1(progresses2, speeds2)).containsExactly(1, 3, 2);
+    }
+}

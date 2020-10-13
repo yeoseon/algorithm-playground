23ae4a8caba1d1ed9a3ff79005f51b3033169e9d
Yeoseon Yun <devyyskr@gmail.com>
2020-10-13 13:20:52 +0900

    refactor(TruckPassingBridgeTest): 불필요한 테스트 코드 삭제

--- src/test/java/Solutions/TruckPassingBridgeTest.java
+++ src/test/java/Solutions/TruckPassingBridgeTest.java
@@ -22,27 +22,7 @@ public class TruckPassingBridgeTest {
         int[] truck_weights3 = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
 
         assertThat(instance.solution1(bridge_length1, weight1, truck_weights1)).isEqualTo(8);
-//        assertThat(instance.solution1(bridge_length2, weight2, truck_weights2)).isEqualTo(101);
-//        assertThat(instance.solution1(bridge_length3, weight3, truck_weights3)).isEqualTo(110);
-    }
-
-    @Test
-    void solution2() {
-        TruckPassingBridge instance = new TruckPassingBridge();
-        int bridge_length1 = 2;
-        int weight1 = 10;
-        int[] truck_weights1 = {7, 4, 5, 6};
-
-        int bridge_length2 = 100;
-        int weight2 = 100;
-        int[] truck_weights2 = {10};
-
-        int bridge_length3 = 100;
-        int weight3 = 100;
-        int[] truck_weights3 = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
-
-        assertThat(instance.solution2(bridge_length1, weight1, truck_weights1)).isEqualTo(8);
-        assertThat(instance.solution2(bridge_length2, weight2, truck_weights2)).isEqualTo(101);
-        assertThat(instance.solution2(bridge_length3, weight3, truck_weights3)).isEqualTo(110);
+        assertThat(instance.solution1(bridge_length2, weight2, truck_weights2)).isEqualTo(101);
+        assertThat(instance.solution1(bridge_length3, weight3, truck_weights3)).isEqualTo(110);
     }
 }

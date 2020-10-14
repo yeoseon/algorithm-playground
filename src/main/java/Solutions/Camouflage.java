eb416cf64ca17d0ba94a4e79b8ce508aee61e22b
Yeoseon Yun <devyyskr@gmail.com>
2020-10-14 16:38:49 +0900

    solve(Camouflage): 위장 풀이

--- src/main/java/Solutions/Camouflage.java
+++ src/main/java/Solutions/Camouflage.java
@@ -1,8 +1,34 @@
 package Solutions;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Camouflage {
+
+    /**
+     * 개수만 리턴하면 되므로 조합의 수를 integer만을 이용해서 구할 수 있다.
+     * (n + 1) * (m + 1) * (o + 1) * (p + 1) - 1 (-1은 하나도 안입는 경우의 수)
+     * https://2ssue.github.io/algorithm/programmers_42578/
+     * @param clothes
+     * @return
+     */
     public int solution1(String[][] clothes) {
-        int answer = 0;
-        return answer;
+        int answer = 1;
+
+        Map<String, Integer> clothesMap = new HashMap<>();
+
+        for(String[] clothe : clothes) {
+            clothesMap.put(clothe[1], clothesMap.getOrDefault(clothe[1], 0) + 1);
+        }
+
+        // 조합 구하기
+        Set<String> keySet = clothesMap.keySet();
+
+        for(String key : keySet) {
+            answer *= clothesMap.get(key) + 1;
+        }
+
+        return answer - 1;
     }
 }

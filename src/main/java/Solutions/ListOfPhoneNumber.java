50219ae3f3da7c8ce01a38eef0ce02cb0b266f77
Yeoseon Yun <devyyskr@gmail.com>
2020-10-15 13:56:37 +0900

    doc(README): 시간복잡도와 공간복잡도 개념 정리 추가

--- src/main/java/Solutions/ListOfPhoneNumber.java
+++ src/main/java/Solutions/ListOfPhoneNumber.java
@@ -2,6 +2,13 @@ package Solutions;
 
 public class ListOfPhoneNumber {
 
+    /**
+     * 1. 하나라도 접두어가 있는게 있으면 다음 절차를 수행하지 않고 False를 반환한다.
+     * 2. 접두어 여부를 판단하는 방법
+     *  2.1
+     * @param phone_book
+     * @return
+     */
     public boolean solution1(String[] phone_book) {
         return false;
     }

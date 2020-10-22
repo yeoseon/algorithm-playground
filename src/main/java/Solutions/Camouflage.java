32200b3a7ce8037891c7ccc41936d81977fc9cb3
Yeoseon Yun <devyyskr@gmail.com>
2020-10-22 11:20:28 +0900

    주석 추가

--- src/main/java/Solutions/Camouflage.java
+++ src/main/java/Solutions/Camouflage.java
@@ -9,6 +9,7 @@ public class Camouflage {
     /**
      * 개수만 리턴하면 되므로 조합의 수를 integer만을 이용해서 구할 수 있다.
      * (n + 1) * (m + 1) * (o + 1) * (p + 1) - 1 (-1은 하나도 안입는 경우의 수)
+     * headgear를 한다면 headgear의 경우의 수는 headgear의 개수 + headgear에서 아예 안하는 개수(1) = 2 + 1
      * https://2ssue.github.io/algorithm/programmers_42578/
      * @param clothes
      * @return
